9323221b6f159d55809fdbc72e9e2df5671b4371
ethanol25 <ethandye04@gmail.com>
2025-11-12 11:58:14 -0600

    update to requirements, added location, seperated into classes

---
+++ Server/Server/Model/Aircraft/NU150.cs
@@ -0,0 +1,18 @@
+﻿namespace Server.Model.Aircraft
+{
+    public class NU150 : IAircraft
+    {
+        public Guid aircraftID { get; }
+        public string aircraftType { get; }
+        public int maxCapacity { get; }
+        public string currentLocation { get; set; }
+        public NU150()
+        {
+            aircraftID = Guid.NewGuid();
+            maxCapacity = 75;
+            aircraftType = "NU-150";
+            currentLocation = string.Empty;
+
+        }
+    }
+}
