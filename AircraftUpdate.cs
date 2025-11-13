71249796085c19ad49ee71e83507ec870ff0c5de
Rushik Prajapati <rushik1553@gmail.com>
2025-11-13 15:13:04 -0600

    Add Model Update Record Interface implementation

---
+++ AircraftUpdate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Airline_Update
+{
+    public class AircraftUpdate : IUpdateRecord
+    {
+        public int UpdateID { get; set; }
+        public DateTime UpdateTime { get; set; }
+        public string Description { get; set; }
+
+        public string AircraftType { get; set; }
+        public string RegistrationNumber { get; set; }
+        public int PassengerCapacity { get; set; }
+
+        
+    }
+}
