a1cf77cf9ddf71a21cde843e2db3ae6c2e915079
06844794 <06844794@ITS-HENZ-PC-008>
2025-11-14 11:38:50 -0600

    boilerplate for datbase connection

---
+++ Server/Server/Model/Database/Database Object.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+
+namespace Server.Model.Database
+{
+    class Program
+    {
+        static void Main()
+        {
+            string connectionString = "Server=YOUR_SERVER_NAME;Database=YOUR_DATABASE_NAME;User Id=YOUR_USERNAME;Password=YOUR_PASSWORD;";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    Console.WriteLine("you are connected to the database");
+
+
+                }
+                catch
+                {
+                    Console.WriteLine("HAHA you failed to connect LOL");
+                }
+            }
+        }
+    }
+}
