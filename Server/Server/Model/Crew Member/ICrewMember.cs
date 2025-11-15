52c172a3256a66b9eba9ad44490faad138042b0a
Ricky-1553 <rprajapati3@unl.edu>
2025-11-15 16:39:18 -0600

    Delete Server/Server directory

--- Server/Server/Model/Crew Member/ICrewMember.cs
+++
@@ -1,9 +0,0 @@
-public interface ICrewMember
-{
-    int CrewMemberID { get; }
-    string FirstName { get; set; }
-    string LastName { get; set; }
-    string Role { get; set; }
-    string Location { get; set; }
-}
-    
