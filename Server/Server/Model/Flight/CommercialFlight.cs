0249fe6571e35832b580852becea24dea790eae9
ethanol25 <ethandye04@gmail.com>
2025-11-21 15:04:04 -0600

    Refactored Flight. Added attributes for EF. Linked to FlightCrew Class, which will handle all of the pilot, firstofficer, flightattendant stuff. Made touchdown stuff nullable, but that kind of depends how/when we are adding them.

--- Server/Server/Model/Flight/CommercialFlight.cs
+++ Server/Server/Model/Flight/CommercialFlight.cs
@@ -1,29 +1,34 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Server.Model.Flight_Crew;
+using Server.Model.Aircraft;
 
 namespace Server.Model.Flight
 {
   public class CommercialFlight : IFlight
   {
-    public CommercialFlight()
-    {
-      FlightGuid = Guid.NewGuid();
-    }
+    [Key]
+    public Guid FlightGuid { get; set; } = Guid.NewGuid();
+    
+    public Guid AircraftId { get; set; }
 
-    public Guid FlightGuid { get; private set; }
-    public Aircraft Plane { get; set; }
-    public CrewMember Pilot { get; set; }
-    public CrewMember FirstOfficer { get; set; }
-    public List<CrewMember> FlightAttendants { get; set; }
+    [ForeignKey("AircraftId")]
+    public virtual Airplane Plane { get; set; } = null!;
 
-    public string Origin { get; set; }
-    public string Destination { get; set; }
+    [Required]
+    public string Origin { get; set; } = string.Empty;
+    [Required]
+    public string Destination { get; set; } = string.Empty;
 
-    public DateTime EstTouchdown { get; set; }
-    public DateTime ActTouchdown { get; set; }
-    public DateTime SchedTouchdown { get; set; }
+    public DateTime? EstTouchdown { get; set; }
+    public DateTime? ActTouchdown { get; set; }
+    public DateTime? SchedTouchdown { get; set; }
 
-    public DateTime EstTakeoff { get; set; }
-    public DateTime ActTakeoff { get; set; }
-    public DateTime SchedTakeoff { get; set; }
+    public DateTime? EstTakeoff { get; set; }
+    public DateTime? ActTakeoff { get; set; }
+    public DateTime? SchedTakeoff { get; set; }
+
+    public virtual ICollection<FlightCrew> CrewAssignments { get; set; } = new List<FlightCrew>();
   }
 }
