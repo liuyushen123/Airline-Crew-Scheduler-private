0882d622698d4807b6e3b501df34482e7c03b4d0
Rushik Prajapati <rushik1553@gmail.com>
2025-11-21 16:27:00 -0600

    update FlightUpdate.cs

--- Server/Server/Model/Update Record/FlightUpdate.cs
+++ Server/Server/Model/Update Record/FlightUpdate.cs
@@ -5,7 +5,7 @@ namespace Server.Model.Update
     public class FlightUpdate : IUpdateRecord
     {
         public int UpdateID { get; set; }
-        public DateTime UpdateTime { get; set; }
+        public DateTime UpdateTime { get; set; } 
         public string Description { get; set; }
     }
 }
