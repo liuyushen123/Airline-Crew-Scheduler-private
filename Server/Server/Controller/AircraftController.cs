40bd150d903b57ff0697452e0a8747e720b53a6c
ethanol25 <ethandye04@gmail.com>
2025-11-29 16:56:30 -0600

    Add controllers for Aircraft, Flights, CrewMembers, and FlightCrew. Used Visual Studio's add scaffolding function.

---
+++ Server/Server/Controller/AircraftController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Server.Model.Aircraft;
+using Server.Model.Database;
+
+namespace Server.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AircraftController : ControllerBase
+    {
+        private readonly AirlineDbContext _context;
+
+        public AircraftController(AirlineDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Aircraft
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Aircraft>>> GetAircraft()
+        {
+            return await _context.Aircraft.ToListAsync();
+        }
+
+        // GET: api/Aircraft/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Aircraft>> GetAircraft(Guid id)
+        {
+            var aircraft = await _context.Aircraft.FindAsync(id);
+
+            if (aircraft == null)
+            {
+                return NotFound();
+            }
+
+            return aircraft;
+        }
+
+        // PUT: api/Aircraft/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAircraft(Guid id, Aircraft aircraft)
+        {
+            if (id != aircraft.AircraftID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(aircraft).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AircraftExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Aircraft
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Aircraft>> PostAircraft(Aircraft aircraft)
+        {
+            _context.Aircraft.Add(aircraft);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAircraft", new { id = aircraft.AircraftID }, aircraft);
+        }
+
+        // DELETE: api/Aircraft/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAircraft(Guid id)
+        {
+            var aircraft = await _context.Aircraft.FindAsync(id);
+            if (aircraft == null)
+            {
+                return NotFound();
+            }
+
+            _context.Aircraft.Remove(aircraft);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AircraftExists(Guid id)
+        {
+            return _context.Aircraft.Any(e => e.AircraftID == id);
+        }
+    }
+}
